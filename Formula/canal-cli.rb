3b017677b6b6d2a2ca4ede303efe0e01ad770516
canal <patryk.jeziorowski@gmx.com>
2021-01-24 14:24:47 +0800

    Brew formula update for cli version v0.0.1

--- Formula/canal-cli.rb
+++ Formula/canal-cli.rb
@@ -10,11 +10,11 @@ class CanalCli < Formula
 
   if OS.mac?
     url "https://github.com/trycanal/cli/releases/download/v0.0.1/cli_0.0.1_darwin_amd64.tar.gz"
-    sha256 "b8ec7cefc3a6b3fcd7e4ee6055a7095a48176a8bdeb9c239c9d8a823c5969317"
+    sha256 "66b85ae6b152079f4aea5f45549665accc7a04a17723ae041becbea5ef8d1abd"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/trycanal/cli/releases/download/v0.0.1/cli_0.0.1_linux_amd64.tar.gz"
-    sha256 "14d95ff8a4337b0921c7bebde4f12317ee44336a576061e764dc0b83abe8b562"
+    sha256 "e2b04c3fed3ba84baeaccd0da414d140504f5aa3d89f8a3af4b012efecfaf274"
   end
 
   def install
